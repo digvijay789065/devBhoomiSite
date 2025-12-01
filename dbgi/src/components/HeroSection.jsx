dc372962b5db2f25642529e4d06591b460ec4621
digvijay789065 <omicronevirus4@gmail.com>
2025-12-02 04:51:09 +0530

    Added Buttons in the Hero Section

--- dbgi/src/components/HeroSection.jsx
+++ dbgi/src/components/HeroSection.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import HeroImage from "../../public/hero-image.webp";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <div>
-      <div className="bg-[url('/hero-image.webp')] h-[90vh] bg-cover bg-center">
-        <div className="bg-[linear-gradient(rgba(0,0,255,0.6),rgba(0,0,255,0.65))] h-[90vh] opacity-70">
-          <div className="flex items-center justify-center">
-            
-            <div className="text-white h-full w-[60vw]">
-              <h1 className="text-5xl font-bold">Shape Your Future With Excellence</h1>
-              <p className="text-xl">
-                Join our vibrant community of learners and innovators at Dev
-                Bhoomi Group of Institutions, where we nurture talent and foster
-                success through world-class education.
-              </p>
-              
-              <div class="hero-btns">
-                <a href="#programs" class="btn">
-                  Explore Programs
-                </a>
-                <a href="admission_form/admission.html" class="btn btn-outline">
-                  Apply Now
-                </a>
-              </div>
-            </div>
-          
+      <div className="h-[90vh] bg-cover bg-center"
+      style={{ backgroundImage: `url(${HeroImage})` }}>
+        <section
+          className="w-full min-h-screen 
+                bg-[linear-gradient(rgba(0,0,255,0.5),rgba(0,0,255,0.55))] 
+                bg-cover bg-center flex flex-col items-center justify-center text-center p-4 md:p-10">
+          <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+            Shape Your Future With Excellence
+          </h1>
+
+          <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl mt-4 max-w-2xl">
+            Join our vibrant community of learners and innovators at Dev Bhoomi
+            Group of Institutions, where we nurture talent and foster success
+            through world-class education.
+          </p>
+
+          <div className="flex gap-4 mt-6 flex-col sm:flex-row">
+            <Link to={''} className="btn">Explore Programs</Link>
+            <Link to={''} className="btn btn-outline">Apply Now</Link>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
